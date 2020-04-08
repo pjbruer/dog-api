382a7a95c1cca17efecaa3a47ed02975ff01a2fa
Jacob Bruér <bruer.pj@gmail.com>
2020-04-08 19:51:36 +0200

    refactor code with single quotes

--- app.js
+++ app.js
@@ -1,29 +1,29 @@
-var express = require("express");
+var express = require('express');
 var api = express();
 
-const hostname = "127.0.0.1";
+const hostname = '127.0.0.1';
 const port = 3000;
 
-api.get("/", (req, res) => {
-    console.log("startpage was called on api");
-    res.status(200).json("try /dogs and /dog/id");
+api.get('/', (req, res) => {
+    console.log('startpage was called on api');
+    res.status(200).json('try /dogs and /dog/id');
 });
 
-api.get("/dogs", (req, res) => {
-    console.log("/dogs was called on api");
-    res.json(["gora", "moss", "fidde"]);
+api.get('/dogs', (req, res) => {
+    console.log('/dogs was called on api');
+    res.json(['gora', 'moss', 'fidde']);
 });
 
 api.get('/dog/:name', (req, res) => {
     var name = req.params.name;
-    if (name == "gora") {
-        console.log("/dog/name was called on api");
-        console.log("param sent in: " + name);
-        res.json({ "name": "gora", "age": "7 years old" })
+    if (name == 'gora') {
+        console.log('/dog/name was called on api');
+        console.log('param sent in: ' + name);
+        res.json({ 'name': 'gora', 'age': '7 years old' })
     } else {
-        console.log("param sent in: " + name);
+        console.log('param sent in: ' + name);
         res.sendStatus(404);
-        res.json("dog was not found, try with the name gora!");
+        res.json('dog was not found, try with the name gora!');
     }
 });
 
